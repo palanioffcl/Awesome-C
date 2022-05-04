f35b3dab033e9f8d0e1f4fcf114a9780cbbe27c4
Palani <84482008+palanioffcl@users.noreply.github.com>
2022-05-04 12:25:58 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+int num;
+scanf("%d",&num);
+for (int i=0; i<= num; i++) {
+for (int j = num; j>1; i++) {
+printf(""); }
+for (j=1; j<= 1; j++) {
+printf("%d",j); }
+printf("\n");
+}}
+
