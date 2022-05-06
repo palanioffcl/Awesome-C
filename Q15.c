967f1a6b37164900d30391023af9073c7e800d52
Palani <84482008+palanioffcl@users.noreply.github.com>
2022-05-06 19:26:54 +0530

    Create Q15.c

---
+++ Q15.c
@@ -0,0 +1,25 @@
+
+
+#include <stdio.h>
+
+void main()
+
+{ 
+
+for (int i=0; i<7; i++) {
+
+if (i == 0) {
+
+printf("A          A\n"); }
+
+else if (i == 1){
+
+printf(" A       A  \n"); }
+
+else if (i == 2) {
+
+printf("   A  A    \n"); }
+
+else if (i == 3 || i == 4 || i== 5 || i== 6) {
+
+printf("     A     \n"); }}}
