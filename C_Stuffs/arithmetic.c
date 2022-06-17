4aa7ae2e2a389fb58fc683103777096ba5e1a82a
Palani <84482008+palanioffcl@users.noreply.github.com>
2022-06-17 17:52:41 +0530

    Create arithmetic.c

---
+++ C_Stuffs/arithmetic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main() {
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("Addition is %d",a+b);
+	printf("Subtraction is %d",a-b);
+	printf("Multiplication is %d",a*b);
+	printf("Division is %d",a/b);
+
+}
