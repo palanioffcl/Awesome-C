57ac18dee91795dcb5086c8eaf23f74cc0b5d7d5
Palani <84482008+palanioffcl@users.noreply.github.com>
2022-06-21 20:18:56 +0530

    Create matrix_transpose.c

---
+++ C_Stuffs/matrix_transpose.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main() {
+    int arr_a[3][3],arr_b[3][3],arr_c[3][3];
+    
+  printf("Enter the Element of A Array");
+    for(int i=0; i<3; i++) {
+      for(int i=0; i<3; i++) {
+            scanf("%d",&arr_a[i][j]);
+      }
+    }
+  
+    for(int i=0; i<3; i++) {
+      for(int i=0; i<3; i++) {
+           printf("%d",arr_c[j][i]);
+      }
+    }
+  
+}
