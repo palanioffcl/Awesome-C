8e6221b2325c6588fd4d4bdc4b3d530afc68fa08
Palani <84482008+palanioffcl@users.noreply.github.com>
2022-06-21 20:15:11 +0530

    Create num_or_alphabet.c

---
+++ C_Stuffs/num_or_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main() {
+        scanf("%d",&in);
+        if(in > 'a' && in < 'z') {
+                        printf("It is a alphabet");
+        else if(in > 0 && in < 9) {
+                        printf("It is an integer");
+        else {
+                        printf("It is an special character");
+}
