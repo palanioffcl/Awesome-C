4f2c0275b4323f2f94c43030d58bd9881a27e89d
Palani <84482008+palanioffcl@users.noreply.github.com>
2022-06-21 20:15:50 +0530

    Create if_inclusions.c

---
+++ C_Stuffs/if_inclusions.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define a 1
+void main() {
+        #if(a >= 1)
+                printf("It is greater than 1");
+        #elif
+                printf("Its not greater than 1");
+        #endif
+}
