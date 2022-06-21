9e0b6e106e4059675136bc18e520b4fdc8cf9f9e
Palani <84482008+palanioffcl@users.noreply.github.com>
2022-06-21 20:06:26 +0530

    Create unary.c

---
+++ C_Stuffs/unary.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main() {
+    int x = 1;
+    printf("Pre increment  %d",++a);
+    printf("Post Increment %d",a++);
+    printf("Pre Decrement  %d",--a);
+    printf("Post Decrement %d",a--);
+}
